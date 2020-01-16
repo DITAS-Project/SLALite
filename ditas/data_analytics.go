9babb4d1198ab00832913303c6008ca21c7d0bed
Rut Palmero <ruth.palmero@atos.net>
2020-01-16 12:49:52 +0100

    Adding comments

--- ditas/data_analytics.go
+++ ditas/data_analytics.go
@@ -78,7 +78,7 @@ func (d DataAnalyticsAdapter) Retrieve(agreement model.Agreement,
 				model.MetricValue{
 					Key:   item.Var.Metric,
 					Value: metricValue,
-//					DateTime: time.Now(),
+					//Adding a two minutes window
 					DateTime: time.Now().Add(time.Minute * -2),
 				},
 			}
@@ -90,10 +90,6 @@ func (d DataAnalyticsAdapter) Retrieve(agreement model.Agreement,
 				"startTime":   item.From.Format(time.RFC3339),
 				"endTime":     item.To.Format(time.RFC3339),
 			}).SetPathParams(map[string]string{
-<<<<<<< HEAD
-				//				"infraId": d.InfraID,
-=======
->>>>>>> c7cd98c5478e3a3319f957cbbd6765d0bf00efc9
 				"infraId": d.VdcID,
 			}).SetResult(&metrics).Get(d.AnalyticsBaseUrl)
 			if err != nil {
